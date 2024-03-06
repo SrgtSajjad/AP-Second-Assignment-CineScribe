76a887756948d6b6f270f99ef00934ac67e6741e
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-06 22:57:07 +0330

    errors in movie class handled

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,31 +8,36 @@ public class Main {
     }
     public static void runMenu() throws IOException {
         // get title from the user
-        System.out.println("Please enter the name of a movie");
         Scanner scanner = new Scanner(System.in);
-
         Movie movie = new Movie(new ArrayList<>(),"",0, "");
-        movie.title = scanner.nextLine();
-        String movieData = movie.getMovieData(movie.title);
+        boolean flag = false;
+        String movieData = "";
+        while (!flag) {
 
-        if (movie.checkResponse(movieData))
-        {
+            System.out.println("Please enter the name of a movie");
+            movie.title = scanner.nextLine();
+            movieData = movie.getMovieData(movie.title);
+            flag = movie.checkResponse(movieData);
+            if (!flag){
+                System.out.println("Error: Movie Not Found");
+            }
+        }
             movie.setAttributes(movieData);
             displayMovieData(movie);
-        }
 
-        else{
-            System.out.println("Error: Movie Not Found");
-        }
+            System.out.println("Choose an actor from the list above");
+
     }
 
     public static void displayMovieData(Movie movie){
         System.out.println("Title: " + movie.title);
         System.out.println("Year(s) Released: " + movie.yearReleased);
         System.out.println("IMDB: " + movie.rating + " from " + movie.ImdbVotes + " votes");
+        System.out.println("Actors: ");
+        int i = 0;
         for (String actor : movie.actorsList) {
-            System.out.println(actor);
+            i++;
+            System.out.println(i + ". " + actor);
         }
-
     }
 }
